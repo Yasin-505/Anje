5727b2858c9194bc2cae54fc6909985e34e7fc04
Yasin-505 <yasin.23392@students.kyu.ac.ke>
2024-09-26 19:06:09 +0300

    Add files via upload

---
+++ yasin.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+	
+	float p,r,t,si;
+	printf("enter the principle amount;/n");
+	scanf("%f",&p);
+	printf("enter the rate");
+	scanf("%f",&r);
+	printf("enter the time in years");
+	scanf("%f",&t);
+//	si=(p*r*t)/100;
+	printf("si,%f",si);
+	return 0;
+}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
