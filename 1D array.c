37c5f9e8b1a141b29b90ed42e9345ac277661163
Yasin-505 <yasin.23392@students.kyu.ac.ke>
2024-10-17 18:35:40 +0300

    Add files via upload

---
+++ 1D array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+	
+	int marks[5] ={34,12,67,54,90};
+	int i;
+	
+	for(i=0;i<5;i++){
+		
+		printf("marks [%d]=%d\n",i,marks[i]);
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
